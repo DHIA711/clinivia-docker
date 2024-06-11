96f3f7ef870bdd9d2038983b89a9293f85805110
DHIA-SIP <sariadhiaeddine@gmail.com>
2024-06-11 22:15:54 +0100

    first-commit

--- clinivia_frontend-master/src/environments/environment.development.ts
+++ clinivia_frontend-master/src/environments/environment.development.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4200',
-  apiDepartements : 'http://localhost:8080/api/Departements'
+  apiDepartements : 'http://localhost:8085/api/Departements'
 };
 
 /*
